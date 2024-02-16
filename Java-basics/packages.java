f19b698d54d178f7fc355864f2465b073e605552
Durga Prasad <131277100+Durga1706@users.noreply.github.com>
2024-02-16 10:27:33 +0530

    Create packages.java

---
+++ Java-basics/packages.java
@@ -0,0 +1,17 @@
+/* Package is like a folder as we create for movies, songs, books etc
+  In simple words, packages in Java are nothing but a folder that contains related files.
+
+  Packages in Java can be categorised into 2 categories.
+   1. Built-in / predefined packages:- java.lang, java.io, java.util etc
+   2. User-defined packages:- Inside  package we can have java files like classes, interfaces and a sub package
+
+ We cn't create two classes with same name but if we create two pckages we can create those in two different packages.
+*/
+
+package myFirstPackage;
+class Main {  
+  public static void main(String args[]) { 
+    System.out.println("Wooohooo!! I created my first package"); 
+  } 
+}
+
