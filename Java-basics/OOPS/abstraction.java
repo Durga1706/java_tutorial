8fc89aba520985b7ec8bcecaa266fdee3bd60bf8
Durga Prasad <131277100+Durga1706@users.noreply.github.com>
2024-08-27 09:26:45 +0530

    Update abstraction.java

--- Java-basics/OOPS/abstraction.java
+++ Java-basics/OOPS/abstraction.java
@@ -65,3 +65,37 @@ class A implements I, Inte{
         System.out.println("Method 3");
     }
 }
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Lambda
+// Use when we have a functional interface(an interface with only one method)
+
+class Main {
+    public static void main(String[] args) {
+        // lambda in single line
+        Animal cat= (n) ->"Hi! I am cat and my name is "+n+"."; 
+        System.out.println(cat.m1("Tom"));
+        
+        Animal dog= (n) ->"Hi! I am dog and my name is "+n+".";
+        System.out.println(dog.m1("Joy"));
+        
+        System.out.println(legs(4, dog, "Joy"));
+        
+        // lambda in multiple lines
+        Animal cow=(n)->{
+         System.out.print("Hi! I am cow and my name is "+n+".");
+         return "I have 4 legs and I give milk.";  // 'return' since we have multiple lines
+        };
+        
+        System.out.println(cow.m1("Jersey"));
+    }
+    
+    static String legs(int n, Animal a, String name){
+        return a.m1(name)+" and I have "+n+" legs";
+    }
+}
+
+interface Animal{
+    String m1(String name);
+}
